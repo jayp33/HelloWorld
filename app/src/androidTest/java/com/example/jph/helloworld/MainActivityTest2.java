0e92b25927e0954332e9a28852e6e77b67881361
JP Hashagen <jayp33e@gmail.com>
2015-08-24 17:26:01 +0200

    Additional Test class

---
+++ app/src/androidTest/java/com/example/jph/helloworld/MainActivityTest2.java
@@ -0,0 +1,18 @@
+package com.example.jph.helloworld;
+
+import junit.framework.TestCase;
+
+/**
+ * Created by jph on 24.08.2015.
+ */
+public class MainActivityTest2 extends TestCase {
+
+    public void setUp() throws Exception {
+        super.setUp();
+
+    }
+
+    public void tearDown() throws Exception {
+
+    }
+}
